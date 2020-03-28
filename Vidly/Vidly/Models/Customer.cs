f04346bddfc52f155475618a73fd6531874d65a6
nangchetnamaw <nangchetnamaw@gmail.com>
2020-03-28 18:28:27 +0530

    added form validations for server side and client side

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -17,6 +17,7 @@ namespace Vidly.Models
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public DateTime? BirthdayDate { get; set; }
