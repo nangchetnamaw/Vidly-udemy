11b3d8be12aa126c6b04d3037801d2a5c8264e99
nangchetnamaw <nangchetnamaw@gmail.com>
2020-03-28 16:58:09 +0530

    added form for adding and editing movies

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.Migrations;
+using Vidly.ViewModels;
 
 
 namespace Vidly.Controllers
@@ -23,6 +25,35 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
         public ViewResult Index()
         {
             var movies = _context.Movies.Include(m => m.Genre).ToList();
@@ -41,7 +72,30 @@ namespace Vidly.Controllers
 
         }
 
-      
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+
+
+
+
 
 
         // GET: Movies/Random
@@ -63,7 +117,7 @@ namespace Vidly.Controllers
             return View(viewModel);          
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult Edit1(int id)
         {
             return Content("id=" + id);
         }
